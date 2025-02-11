c7f9867187640c9f508fe4a6f78373d50b895c61
yeong <chlgkdud111@gmail.com>
2025-02-11 19:25:33 +0900

    Add: 이메일 인증을 위한 기능 구현

---
+++ apps/crud/utils.py
@@ -0,0 +1,43 @@
+import smtplib
+import random
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+import os
+from app import app
+
+def send_verification_eamil(to_email):
+  load_dotenv()
+
+  # 환경 변수에서 이메일 정보 가져오기
+  smtp_server = os.getenv('MAIL_SERVER')
+  smtp_port = os.getenv('MAIL_PORT')
+  sender_email = os.getenv('MAIL_USERNAME')
+  sender_password = os.getenv('MAIL_PASSWORD')
+
+  # 6자리 인증번호 생성
+  verification_code = random.randint(100000, 999999)
+
+  # 이메일 내용 설정
+  subject = '[숨하] 이메일 인증번호'
+  body = f"안녕하세요. 숨하입니다. \n인증 번호를 입력하여 이메일 인증을 완료해주세요. \n인증 번호 : {verification_code}"
+
+  msg = MIMEMultipart()
+  msg['From'] = sender_email
+  msg['To'] = to_email
+  msg['Subject'] = subject
+  msg.attach(MIMEText(body, 'plain'))
+
+  # SMTP 서버와 연결하여 이메일 보내기
+  try:
+      server = smtplib.SMTP(smtp_server, smtp_port)
+      server.starttls()  # TLS 연결 보안 활성화
+      server.login(sender_email, sender_password)
+      server.sendmail(sender_email, to_email, msg.as_string())
+      server.quit()
+
+      # 인증번호 반환 (임시 저장)
+      return verification_code
+  except Exception as e:
+      print(f"이메일 전송 실패: {e}")
+      return None
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -3,8 +3,11 @@ from werkzeug.security import generate_password_hash
 from apps.crud.models import db, User, Level
 from apps.crud.enums import UserRole
 from apps.crud.forms import UserForm
+from utils import send_verification_eamil
 
 bp = Blueprint("crud", __name__, static_folder="static")
+
+stored_verification_code = None
 
 @bp.route('/signup', methods=["POST"])
 def signup():
@@ -64,3 +67,42 @@ def signup():
   db.session.commit()
 
   return jsonify({"message": "사용자 정보가 잘 저장되었습니다."}), 201
+
+
+# 이메일 인증번호 전송 API
+@bp.route('/send_email_code', methods=['POST'])
+def send_email_code():
+  data = request.get_json()
+  email = data.get('email')
+
+  # 이메일이 제공되지 않은 경우 처리
+  if not email:
+    return jsonify({"error": "이메일 주소를 제공해주세요."}), 400
+  
+  # 이메일로 인증번호 전송
+  verification_code = send_verification_eamil(email)
+
+  if verification_code:
+    # 인증번호를 임시 저장
+    global stored_verification_code
+    stored_verification_code = verification_code
+    return jsonify({"message": "인증번호가 이메일로 전송되었습니다."}), 200
+  else:
+    return jsonify({"error": "이메일 전송 실패"}), 500
+
+  
+# 인증번호 검증 API
+@bp.route('/verify_email_code', methods=['POST'])
+def verify_email():
+  data = request.get_json()
+  input_code = data.get('emailCode')
+
+  # 인증번호 입력되지 않은 경우 처리
+  if not input_code:
+      return jsonify({"error": "인증번호를 입력해주세요."}), 400
+
+  # 인증번호 검증
+  if input_code == stored_verification_code:
+      return jsonify({"message": "인증번호가 확인되었습니다."}), 200
+  else:
+      return jsonify({"error": "인증번호가 일치하지 않습니다."}), 400
