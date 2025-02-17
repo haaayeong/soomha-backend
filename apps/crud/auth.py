b8ea3f2ab96a536d9929f4e2a855e7a4cbff5d32
yeong <chlgkdud111@gmail.com>
2025-02-17 12:02:16 +0900

    Add: 로그인 한 사용자 정보 조회하는 API 구현 Fixed: 로그인 시도 중 발생하는 에러 수정

--- apps/crud/auth.py
+++ apps/crud/auth.py
@@ -18,10 +18,32 @@ def login():
   
   user = User.query.filter_by(username=username).first()
 
-  if not user or not check_password_hash(user.password, password):
+  if not user or not user.verify_password(password):
     return jsonify({"error": "아이디 또는 비밀번호가 올바르지 않습니다."})
   
   # JWT 토큰 생성 (유효기간 3시간)
   access_token = create_access_token(identity=user.id, expires_delta=timedelta(hours=3))
 
   return jsonify({"message": "로그인 성공", "token": access_token}), 200
+
+# 로그인한 사용자 정보 조회
+@bp.route("/user", methods=["GET"])
+@jwt_required()
+def get_user():
+  user_id = get_jwt_identity()
+  user = User.query.get(user_id)
+
+  if not user:
+    return jsonify({"error": "사용자를 찾을 수 없습니다."}), 404
+  
+  return jsonify({
+    "id": user.id,
+    "username": user.username,
+    "profile_image": user.profile_image,
+    "nickname": user.nickname,
+    "email" : user.email,
+    "kindergarten" : user.kindergarten,
+    "stamp" : user.stamp,
+    "level_id" : user.level_id,
+    "area" : user.area
+  }), 200
--- apps/crud/models.py
+++ apps/crud/models.py
@@ -39,6 +39,7 @@ class User(db.Model):
   def password(self, password):
     self.password_hash = generate_password_hash(password)
 
+  # 비밀번호 검증
   def verify_password(self, password):
     return check_password_hash(self.password_hash, password)
   
